50eb601a64d0033d2af26759db8d5931525317f5
catclou <caterina.moccia@gmail.com>
2022-03-30 14:58:08 +0200

    esercizio completato

---
+++ assets/JS/main.js
@@ -0,0 +1,11 @@
+// ISTANZIARE VUE:
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            welcome: 'Weee, alooora?',
+            img: './assets/IMG/procione.jpg',
+            altImg : 'un procione che ti saluta'
+        }
+    }
+)
